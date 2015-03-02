16a6134f0fef242d9c7ece202904bb1cac502005
Joshua Randall <jcrandall@alum.mit.edu>
2015-03-02 14:12:20 +0000

    Reverts accidentally commented out scratch113

--- lustre_reports/scripts/treemap.js
+++ lustre_reports/scripts/treemap.js
@@ -277,7 +277,7 @@ define(["d3", "lodash", "queue"], function(d3, _, queue) {
     var path_data_url_templates = {
 	//	"/lustre/scratchtest": _.template("../api/lustretree/scratchtest?depth=2&path=<%= path %>"),
 	"/lustre/scratch111": _.template("../api/lustretree/scratch111?depth=2&path=<%= path %>"),
-//	"/lustre/scratch113": _.template("../api/lustretree/scratch113?depth=2&path=<%= path %>"),
+	"/lustre/scratch113": _.template("../api/lustretree/scratch113?depth=2&path=<%= path %>"),
 	"/lustre/scratch114": _.template("../api/lustretree/scratch114?depth=2&path=<%= path %>"),
     }
 
